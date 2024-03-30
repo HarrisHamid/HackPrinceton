c4d1233638452b3b6982db4c2b68e81b54024167
miguel-merlin <mmerlin@stevens.edu>
2024-03-30 02:24:47 -0400

    feat: Added theme provider

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React from 'react';
+import { ConfigProvider } from 'antd';
 
 function App() {
   return (
-    <div className="main-container">
-      <div className="header">
-        <h1>polyglot</h1>
+    <ConfigProvider theme={{token: {colorPrimary: '#00b96b'}}}>
+      <div className="App">
+        <h1>Hello World</h1>
       </div>
-      <div className="grid-container">
-        <div className="card">Card 1</div>
-        <div className="card">Card 2</div>
-        <div className="card">Card 3</div>
-        <div className="card">Card 4</div>
-        {/* Add more cards as needed */}
-      </div>
-    </div>
+    </ConfigProvider>
   );
 }
 
