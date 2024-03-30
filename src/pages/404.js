1ed8716b991d879bde23ab9e76cf0ef85ffe11dd
miguel-merlin <mmerlin@stevens.edu>
2024-03-30 03:17:40 -0400

    feat: Added 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React from 'react';
+import { Result } from 'antd';
 
 const NotFound = () => {
     return (
-        <div>
-            <h1>404</h1>
-        </div>
+        <Result
+            status="404"
+            title="404"
+            subTitle="Sorry, the page you visited does not exist."
+
+        />
     );
-}
+};
 
 export default NotFound;
