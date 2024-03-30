58c141a89f57c55f6e2c9c95e145d07b5f2e02b0
Harris Hamid <harrishamid26@gmail.com>
2024-03-30 16:22:51 -0400

    fixed runtime error for progress button

--- src/components/StoryCard.js
+++ src/components/StoryCard.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card } from 'antd';
+import { useNavigate } from 'react-router';
+import { useAuth0 } from "@auth0/auth0-react";
+import NotLoggedIn from './NotLoggedIn';
 
 const { Meta } = Card;
 
-const StoryCard = ({ title, imageUrl, storyId,onCardClick  }) => {
+const StoryCard = ({ title, imageUrl, storyId }) => {
+  const navigate = useNavigate();
+  const { isAuthenticated } = useAuth0();
+  const [showAlert, setShowAlert] = useState(false);
+
+  const handleCardClick = () => {
+    if (isAuthenticated) {
+      navigate(`/story/${storyId}`);
+    } else {
+      setShowAlert(true); // Set showAlert to true to display the error message
+    }
+  };
+
   return (
-    <Card
-      hoverable
-      style={{ width: 240 }}
-      cover={<img alt={title} src={imageUrl} />}
-      onClick={onCardClick} 
-    >
-      <Meta title={title} />
-    </Card>
+    <>
+      {showAlert && <NotLoggedIn />}
+      <Card
+        hoverable
+        style={{ width: 240 }}
+        cover={<img alt={title} src={imageUrl} />}
+        onClick={handleCardClick}
+      >
+        <Meta title={title} />
+      </Card>
+    </>
   );
 };
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,34 +1,18 @@
 import React, { useState } from "react";
-import { Layout, Row, Col, Modal } from "antd";
+import { Layout, Row, Col } from "antd";
 import { useNavigate } from "react-router";
 import Sidebar from "../components/Sidebar";
 import Navbar from "../components/Navbar";
 import StoryCard from "../components/StoryCard";
-import { useAuth0 } from "@auth0/auth0-react";
-import NotLoggedIn from "../components/NotLoggedIn"; 
 
 const { Content } = Layout;
 
 const Home = () => {
   const [collapsed, setCollapsed] = useState(false);
   const navigate = useNavigate();
-  const [isModalVisible, setIsModalVisible] = useState(false); 
-  const { isAuthenticated } = useAuth0(); 
 
   const toggleSidebar = () => {
     setCollapsed(!collapsed);
-  };
-
-  const handleCardClick = (storyId) => {
-    if (isAuthenticated) {
-      navigate(`/story/${storyId}`);
-    } else {
-      setIsModalVisible(true);
-    }
-  };
-
-  const handleOk = () => {
-    setIsModalVisible(false);
   };
 
   const layoutStyle = {
@@ -61,7 +45,11 @@ const Home = () => {
 
   return (
     <Layout style={layoutStyle}>
-      <Sidebar collapsed={collapsed} onCollapse={setCollapsed} />
+      <Sidebar
+        collapsed={collapsed}
+        onCollapse={setCollapsed}
+        navigate={navigate}
+      />
       <Layout>
         <Navbar isSidebarCollapsed={collapsed} toggleSidebar={toggleSidebar} />
         <Content style={contentStyle}>
@@ -69,27 +57,12 @@ const Home = () => {
             Enjoy your next story
           </h2>
           <Row gutter={[16, 16]}>
-            {stories.map((story) => (
-              <Col key={story.id} xs={24} sm={12} md={8} lg={6}>
-                <StoryCard
-                  title={story.title}
-                  imageUrl={story.imageUrl}
-                  storyId={story.id}
-                  onCardClick={() => handleCardClick(story.id)} 
-                />
+            {stories.map((story, index) => (
+              <Col key={index} xs={24} sm={12} md={8} lg={6}>
+                <StoryCard title={story.title} imageUrl={story.imageUrl} storyId={story.id} />
               </Col>
             ))}
           </Row>
-          {/* Modal should be included here */}
-          <Modal
-            title="Error"
-            visible={isModalVisible}
-            onOk={handleOk}
-            onCancel={handleOk}
-            footer={null}
-          >
-            <NotLoggedIn />
-          </Modal>
         </Content>
       </Layout>
     </Layout>
