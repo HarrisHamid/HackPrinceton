0f58c93448928f82ddb9f52f7e5a710e6a55be9d
miguel-merlin <mmerlin@stevens.edu>
2024-03-30 02:41:57 -0400

    feat: Added API endpoints

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,19 @@
 import axios from 'axios';
 
-export default axios.create({
-    baseUrl: `http://18.220.29.105`
+const apiClient = axios.create({
+    baseUrl: `http://18.220.29.105`,
+    headers: {
+        'Content-Type': 'application/json'
+    }
 })
+
+const request = async (method, url, data) => {
+    try {
+        const response = await apiClient[method](url, data);
+        return response.data;
+    } catch (error) {
+        return error.response.data;
+    }
+}
+
+export default request;
