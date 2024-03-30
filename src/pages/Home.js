fa02dbad41c13371d2ec538818a07b7dd4bc8922
miguel-merlin <mmerlin@stevens.edu>
2024-03-30 03:11:21 -0400

    feat: Added sidebar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import { Layout, Menu, Button } from "antd";
+import { UserOutlined, MenuUnfoldOutlined, MenuFoldOutlined } from "@ant-design/icons";
+import { useNavigate } from "react-router";
+
+const { Header, Sider, Content } = Layout;
 
 const Home = () => {
-    return (
-        <div>
-            <h1>Hello World</h1>
-        </div>
-    );
-}
+  const [collapsed, setCollapsed] = useState(false);
+  const navigate = useNavigate();
+//   const {
+//     token: { colorBgContainer },
+//   } = theme.useToken();
+  const handleClickHome = () => {
+    navigate('/');
+  }
+
+  return (
+    <Layout>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="demo-logo-vertical" />
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          items={[
+            {
+              key: '1',
+              icon: <UserOutlined />,
+              label: 'Home',
+              onClick: handleClickHome,
+            },
+          ]}
+        />
+      </Sider>
+      <Layout>
+        <Header>
+          <Button
+            type="text"
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            onClick={() => setCollapsed(!collapsed)}
+            style={{
+              fontSize: '16px',
+              width: 64,
+              height: 64,
+            }}
+          />
+        </Header>
+        <Content>
+          Content
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
 
 export default Home;
