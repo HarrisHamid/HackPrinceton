44838003999596dfa9dc07146a8f6938fc9938d0
miguel-merlin <mmerlin@stevens.edu>
2024-03-30 20:52:06 -0400

    feat: Removed button from Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Layout, Button } from 'antd';
-import { MenuUnfoldOutlined, MenuFoldOutlined } from '@ant-design/icons';
+import { Layout } from 'antd';
 import LoginButton from './LoginButton';
 import LogoutButton from './LogoutButton';
 
@@ -9,15 +8,6 @@ const { Header } = Layout;
 const Navbar = ({ isSidebarCollapsed, toggleSidebar }) => {
   return (
     <Header style={{ padding: 0, background: '#fff', display: 'flex', alignItems: 'center' }}>
-      <Button
-        type="text"
-        icon={isSidebarCollapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-        onClick={toggleSidebar}
-        style={{
-          marginLeft: '20px',
-          fontSize: '16px',
-        }}
-      />
       <div style={{ marginLeft: '20px' }}>
         <h2>Polyglot</h2>
       </div>
