f8708b386d01ce9dcff444eb86d6f4c72761777b
miguel-merlin <mmerlin@stevens.edu>
2024-03-30 21:13:34 -0400

    feat: Fixed navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import { Layout } from 'antd';
-import LoginButton from './LoginButton';
-import LogoutButton from './LogoutButton';
+import React from "react";
+import { Layout } from "antd";
+import LoginButton from "./LoginButton";
+import LogoutButton from "./LogoutButton";
+import logo from "../static/polyglot_logo.png";
 
 const { Header } = Layout;
 
-const Navbar = ({ isSidebarCollapsed, toggleSidebar }) => {
+const Navbar = () => {
   return (
-    <Header style={{ padding: 0, background: '#fff', display: 'flex', alignItems: 'center' }}>
-      <div style={{ marginLeft: '20px' }}>
+    <Header
+      style={{
+        padding: 0,
+        background: "#fff",
+        display: "flex",
+        alignItems: "center",
+      }}
+    >
+      <div
+        style={{ display: "flex", alignItems: "center", marginLeft: "20px" }}
+      >
+        <img
+          src={logo}
+          alt="Logo"
+          style={{ width: "40px", height: "40px", marginRight: "2px" }}
+        />
         <h2>Polyglot</h2>
       </div>
-      <div style={{ flex: 1, display: 'flex', justifyContent: 'flex-end' }}>
+      <div style={{ flex: 1, display: "flex", justifyContent: "flex-end" }}>
         <LoginButton />
         <LogoutButton />
       </div>
